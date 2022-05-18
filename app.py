5210ee2f55b43010413370f14fe738dc17ed0a4f
1210168 <103553452+1210168@users.noreply.github.com>
2022-05-18 13:58:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():     #index関数の定義
+def index():
     return render_template("index.html")
 
 @app.route("/article1")
-def article1():　#article1関数の定義　
+def article1():　
     return render_template("article1.html")
 
 @app.route("/article2")
-def article2():　#article2関数の定義
+def article2():
     return render_template("article2.html")
 
 if __name__ == '__main__':

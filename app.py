50202bb7f94c67d6505f9654d53b8b87f8b2dadd
1210168 <cbmh1210168@gn.iwasaki.ac.jp>
2022-05-25 16:27:19 +0900

    update

--- app.py
+++ app.py
@@ -60,6 +60,8 @@ def update(id):
 def delete(id):
     root = tk.Tk()
     root.withdraw()
+    root.attributes('-topmost', True)
+    root.lift()
     root.focus_force()
     res = messagebox.askokcancel("確認", "投稿を削除しますか？")
     if res == True:
